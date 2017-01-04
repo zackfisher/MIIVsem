b824d7388bdcd6da96d79f7aaf1920a0cac8fb91
Mikko Rönkkö <mikko.ronkko@jyu.fi>
2017-01-04 18:27:52 +0200

    Moved examples as separate files

---
+++ example/bollen1989-miive2.R
@@ -0,0 +1,17 @@
+# Example 2
+
+my_instruments <- ' 
+    y1 ~ x2 + x3                            
+y5 ~ y2 + y3 + y4 + x2                
+y2 ~ y3 + y7 + y8 + x2           
+y3 ~ y2 + y4 + y6 + y8        
+y4 ~ y3 + y6           
+y6 ~ y3 + y4 + y7 + x2            
+y7 ~ y2 + y4 + y6 + y8       
+y8 ~ y2 + y3 + y7 + x2          
+x2 ~ y1 + y5 + y2 + y3 + y4 + y6
+x3 ~ y1 + y5 + y2 + y3 + y4 + y6
+'
+
+miive(model = bollen1989a_model, data = bollen1989a, 
+      instruments = my_instruments)

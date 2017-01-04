7e27063a60f596632ac08c2129aa84035523615a
Mikko Rönkkö <mikko.ronkko@jyu.fi>
2017-01-04 17:10:15 +0200

    Moved example to a separate file. This makes the code a bit more modular and allows reusing the examples easier.

--- R/miivs.R
+++ R/miivs.R
@@ -96,31 +96,11 @@
 #' 	Bentler, P. M., and Weeks, D. G. (1980). Linear structural equations with 
 #' 	latent variables. Psychometrika, 45(3), 289–308.                 
 #' 	
-#'
-#' @examples
-#'  bollen1989a_model <- '
-#'
-#'    Eta1 =~ y1 + y2  + y3  + y4  
-#'    Eta2 =~ y5 + y6  + y7  + y8    
-#'    Xi1  =~ x1 + x2 + x3 
-#'
-#'    Eta1 ~ Xi1  
-#'    Eta2 ~ Xi1 
-#'    Eta2 ~ Eta1 
-#'
-#'    y1   ~~ y5
-#'    y2   ~~ y4
-#'    y2   ~~ y6
-#'    y3   ~~ y7
-#'    y4   ~~ y8
-#'    y6   ~~ y8 
-#'  '
-#'  
-#'  miivs(bollen1989a_model)
-#'  
+#' @example example/bollen1989a.R
+#'   
 #' @export
 
-miivs <- function(model, miivs.out = FALSE){
+miivs <- function(model){
 
   
   pt <- lavaanify(model, auto = TRUE)

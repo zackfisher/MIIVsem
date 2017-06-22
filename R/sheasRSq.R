cd9f635a3699ca5b071dbb57c10d8c2c35d0eaf5
‘zackfisher’ <fish.zachary@gmail.com>
2017-06-22 16:09:37 -0400

    Equation-specific reformulation of Shea's Partial.

--- R/sheasRSq.R
+++ R/sheasRSq.R
@@ -3,50 +3,46 @@
 #' @param object a fitted miive object.
 #' 
 #'@keywords internal
-sheasRSq <- function(object){
+sheasRSq <- function(IVobs, MIIVs, XX1, sample.cov, sample.mean, sample.nobs){
   
-  M <- object$sample.mean
-  N <- object$sample.nobs
+  M <- sample.mean
+  N <- sample.nobs
   # undo smaple.cov rescale to match Stata results
-  S <- object$sample.cov * N/(N-1)
+  S <- sample.cov * N/(N-1)
   
-  object$eqn <- lapply(x$eqn, function(eq){
-    
-    if (length(eq$IVobs) == 1){
+  if (length(IVobs) == 1){
       
-        sheasRSq    <- t(S[eq$IVobs,eq$MIIVs]) %*% solve(S[eq$MIIVs, eq$MIIVs]) %*% 
-                         S[eq$IVobs,eq$MIIVs] %*% solve(S[eq$IVobs, eq$IVobs])
-        sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(eq$MIIVs))
-        shea <- cbind(sheasRSq, sheasAdjRSq)
-        rownames(shea) <- y
-        shea
+    sheasRSq    <- t(S[IVobs,MIIVs]) %*% solve(S[MIIVs, MIIVs]) %*% 
+                     S[IVobs,MIIVs] %*% solve(S[IVobs, IVobs])
     
-    } else {
+    sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(MIIVs))
+    shea <- cbind(sheasRSq, sheasAdjRSq)
+    rownames(shea) <- y
     
-      sheas <- do.call("rbind", lapply(eq$IVobs, function(y){
+  } else {
+    
+    shea <- do.call("rbind", lapply(IVobs, function(y){
         
-        x2 <- setdiff(eq$IVobs, y)
+      x2 <- setdiff(IVobs, y)
       
-        M1 <- (eq$XX1[c(y,x2),c(y,x2)] - N * M[c(y,x2)] %*% t(M[c(y,x2)]) ) / (N - 1)
+      M1 <- (XX1[c(y,x2),c(y,x2)] - N * M[c(y,x2)] %*% t(M[c(y,x2)]) ) / (N - 1)
       
-        XXt <- S[y,y] -  2*(S[y,x2] %*%  solve(S[x2,x2]) %*% S[x2,y]) +  
-               S[y,x2] %*% solve(S[x2,x2]) %*% S[x2,y] 
+      XXt <- S[y,y] -  2*(S[y,x2] %*%  solve(S[x2,x2]) %*% S[x2,y]) +  
+             S[y,x2] %*% solve(S[x2,x2]) %*% S[x2,y] 
 
-        XXb <- M1[y,y] - 2*(M1[y,x2] %*% solve(M1[x2,x2]) %*% M1[x2,y]) + 
-               M1[y,x2] %*% solve(M1[x2,x2]) %*% M1[x2,y] 
+      XXb <- M1[y,y] - 2*(M1[y,x2] %*% solve(M1[x2,x2]) %*% M1[x2,y]) + 
+             M1[y,x2] %*% solve(M1[x2,x2]) %*% M1[x2,y] 
         
-        sheasRSq    <- XXb %*% solve(XXt)
-        sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(eq$MIIVs))
-        shea <- cbind(sheasRSq, sheasAdjRSq)
-        rownames(shea) <- y
-        shea
-      }))
+      sheasRSq    <- XXb %*% solve(XXt)
+      sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(MIIVs))
+        
+      sheas <- cbind(sheasRSq, sheasAdjRSq)
+      rownames(sheas) <- y
+      sheas
       
-      colnames(sheas) <-c("sheasPartRSq", "sheasPartAdjRSq")
+    }))
     
-    }
-  
-  })
-  
-  return(object)
+  }
+  colnames(shea) <-c("sheasPartRSq", "sheasPartAdjRSq")
+  return(shea)
 }

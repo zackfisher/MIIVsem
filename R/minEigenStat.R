4a4f6e6b98900c31e7f0dd61047b2101510e7284
‘zackfisher’ <fish.zachary@gmail.com>
2017-06-23 17:29:23 -0400

    Added Cragg-Donald minimum eigenvalue statistic.

---
+++ R/minEigenStat.R
@@ -0,0 +1,41 @@
+#' Calculate the minimum eigenvalue statistic of Cragg and Donald (1993)
+#'
+#' @param IVobs a vector of endogenous RHS variables.
+#' @param MIIVs a vector of instrumental variables.
+#' @param data matrix or data.frame containing raw data.
+#' 
+#'@keywords internal
+minEigenStat <- function(IVobs, MIIVs, data){
+  
+  if (is.null(data)){
+    stop("Raw data required for minimum eigenvalue statistic.")
+  }
+  
+  # Define operator for taking matrix powers and other functions
+  "%^%" <- function(x, n) with(eigen(x), vectors %*% (values^n * t(vectors)))
+  M     <- function(A){diag(nrow(A)) - A %*% solve(t(A) %*% A) %*% t(A)}
+
+  if (length(intersect(IVobs, MIIVs)) > 0 ){
+    
+      X <- cbind(1, data[,intersect(IVobs, MIIVs), drop = FALSE])
+      Z <- data[,MIIVs[!MIIVs %in% intersect(IVobs, MIIVs)], drop = FALSE]
+      
+  } else {
+    
+      X <- matrix(1, nrow(data), 1)
+      Z <- data[,MIIVs, drop = FALSE]
+  }
+  
+  MX1 <- M(X)
+  Zb  <- cbind(X, Z)
+  MZ  <- M(Zb) 
+  Kz  <- ncol(Zb) - 1
+
+  SigVV   <- (t(Y) %*% MZ %*% Y) * (1 / (nrow(data)-Kz))
+  SigVV.5 <- SigVV %^% (-0.5)
+  G <- t(SigVV.5) %*% t(Y) %*% t(MX1) %*% Z %*% solve(t(Z) %*% MX1 %*% Z) %*% 
+       t(Z) %*% MX1 %*% Y %*% (SigVV.5) * (1/Kz)
+
+  minEigen <- min(eigen(G)$values) 
+  return(minEigen)
+}
--- R/sheasRSq.R
+++ R/sheasRSq.R
@@ -1,23 +1,40 @@
 #' Calculate Shea's Partial R-squares
 #'
-#' @param object a fitted miive object.
+#' @param IVobs a vector of endogenous RHS variables.
+#' @param MIIVs a vector of instrumental variables.
+#' @param sample.cov Numeric matrix. A sample variance-covariance 
+#'        matrix. The rownames and colnames attributes must contain all
+#'        the observed variable names indicated in the model syntax.
+#' @param sample.mean A sample mean vector. If \code{sample.cov} is provided
+#'        and the \code{sample.mean} argument is \code{NULL}, intercepts
+#'        for all endogenous variables will not be estimated. 
+#' @param sample.nobs Number of observations in the full data frame. 
 #' 
 #'@keywords internal
-sheasRSq <- function(IVobs, MIIVs, XX1, sample.cov, sample.mean, sample.nobs){
+sheasRSq <- function(IVobs, MIIVs, sample.cov, sample.mean, sample.nobs){
   
   M <- sample.mean
+  
   N <- sample.nobs
-  # undo smaple.cov rescale to match Stata results
-  S <- sample.cov * N/(N-1)
+  
+  S <- sample.cov 
+  
+  sVV <- chol2inv(chol(sample.cov[MIIVs, MIIVs]))
+
+  ZV  <- sample.cov[IVobs , MIIVs, drop = FALSE]
+
+  XX1 <- ZV %*% sVV %*% t(ZV)
   
   if (length(IVobs) == 1){
       
     sheasRSq    <- t(S[IVobs,MIIVs]) %*% solve(S[MIIVs, MIIVs]) %*% 
-                     S[IVobs,MIIVs] %*% solve(S[IVobs, IVobs])
+                     S[IVobs,MIIVs]  %*% solve(S[IVobs, IVobs])
     
     sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(MIIVs))
+    
     shea <- cbind(sheasRSq, sheasAdjRSq)
-    rownames(shea) <- y
+    
+    rownames(shea) <- IVobs
     
   } else {
     
@@ -25,7 +42,8 @@ sheasRSq <- function(IVobs, MIIVs, XX1, sample.cov, sample.mean, sample.nobs){
         
       x2 <- setdiff(IVobs, y)
       
-      M1 <- (XX1[c(y,x2),c(y,x2)] - N * M[c(y,x2)] %*% t(M[c(y,x2)]) ) / (N - 1)
+      #M1 <- (XX1[c(y,x2),c(y,x2)] - N * M[c(y,x2)] %*% t(M[c(y,x2)]) ) / (N - 1)
+      M1 <- XX1[c(y,x2),c(y,x2)] 
       
       XXt <- S[y,y] -  2*(S[y,x2] %*%  solve(S[x2,x2]) %*% S[x2,y]) +  
              S[y,x2] %*% solve(S[x2,x2]) %*% S[x2,y] 
@@ -34,15 +52,21 @@ sheasRSq <- function(IVobs, MIIVs, XX1, sample.cov, sample.mean, sample.nobs){
              M1[y,x2] %*% solve(M1[x2,x2]) %*% M1[x2,y] 
         
       sheasRSq    <- XXb %*% solve(XXt)
+      
       sheasAdjRSq <- 1 - (1 - sheasRSq)*(N-1)/(N - length(MIIVs))
         
       sheas <- cbind(sheasRSq, sheasAdjRSq)
+      
       rownames(sheas) <- y
+      
       sheas
       
     }))
     
   }
+  
   colnames(shea) <-c("sheasPartRSq", "sheasPartAdjRSq")
+  
   return(shea)
+  
 }
